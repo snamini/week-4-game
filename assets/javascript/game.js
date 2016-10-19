5a05fdde42b57fc6a58ef65015b136722476c5de
Serene Talia Namini <serene@Serenes-MacBook-Pro.local>
2016-10-18 20:00:58 -0700

    hi

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,6 @@ var ruby = 0;
 var diamond = 0;
 var quartz = 0;
 
-var userClick= null;
-var lessthantargetnumber = 0;
-var equaltotargetnumber = 0;
-var overtargetnumber = 0;
-
     // Game Counters
 
 var wins = 0;
@@ -22,29 +17,25 @@ var totalscore = 0;
 // FUNCTIONS (resusable blocks of code that I will call upon when needed )
 // ------------------------------------------------------------------------------------------------------
 
-
+// start of game: regenerating a new random number, displaying the current wins and losses, and generate new random numbers for the crystals
 
 // function to give you a random number at start
 function startGame () {
 	random = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    amethyst = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    ruby = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    diamond = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    quartz = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 }
 
-
-// <!-- onclickfunction: 
-// use variable to record what number the user clicks -->
-make this into an onclickfunction
-
-$( "#target" ).click(function() {
+$( "#crystal" ).click(function() {
   alert( "Handler for .click() called." );
 });
 
-// function to set the pictures equal to a random number
-function secondfunction(amethyst, ruby, diamond, quartz) {
-amethyst = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-ruby = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-diamond = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-quartz = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-}
+
+
+
+
 
 
 // Testing/Debugging
@@ -64,20 +55,20 @@ var randomnumber= "????"
     startGame();
 
 
-Register Keyclicks
+// Register Keyclicks
 
-document.onkeyup = function(event) {
+document.onClick = function(event) {
     var amethyst = String.fromCharCode(event.keyCode).toLowerCase();
 }
 
-document.onkeyup = function(event) {
+document.onClick = function(event) {
     var ruby = String.fromCharCode(event.keyCode).toLowerCase();
 }
-document.onkeyup = function(event) {
+document.onClick = function(event) {
     var diamond = String.fromCharCode(event.keyCode).toLowerCase();
 }
-document.onkeyup = function(event) {
+document.onClick = function(event) {
     var quartz = String.fromCharCode(event.keyCode).toLowerCase();
 }
 
-console.log(amethyst,);
+console.log(amethyst);
