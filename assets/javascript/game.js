74d32130a08dcfcc223cce0050cb5eae2db3b490
Serene Talia Namini <serene@Serenes-MacBook-Pro.local>
2016-10-18 17:05:49 -0700

    meow

---
+++ assets/javascript/game.js
@@ -0,0 +1,125 @@
+// // <!-- psuedo-code:
+
+// //  --> once the game begins:
+
+// create a function to hold the whole entire game
+function game() {
+
+
+// <!--  declare variables for objects -->
+
+var random = 0;
+var amethyst = 0;
+var ruby = 0;
+var diamond = 0;
+var quartz = 0;
+
+var userClick= null;
+
+// <!--  generate a random number in a function  -->
+
+
+function firstfunction() {
+random = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+}
+
+console.log(random);
+
+function secondfunction(amethyst, ruby, diamond, quartz) {
+amethyst = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+ruby = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+diamond = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+quartz = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+}
+console.log(amethyst, ruby, diamond, quartz);
+();
+
+console.log(random);
+
+// <!-- var for each player score -->
+
+var = playerScore = 0;
+
+
+
+var lessthantargetnumber = 0;
+
+var equaltotargetnumber = 0;
+
+var overtargetnumber = 0;
+
+
+<!-- 
+// var for game status -->
+
+var wins = 0
+
+var losses = 0
+
+// create a function for the whole entire game
+function(game) {}
+
+
+// <!-- onclickfunction: 
+// use variable to record what number the user clicks -->
+
+THEN
+
+// if attribute is ruby, then increase player score by 4
+// if attribute is ruby, then increase player score by 6
+// if attribute is ruby, then increase player score by 2
+// if attribute is ruby, then increase player score by 4
+
+THE IMAGES HAVE TO BE RANDOM BETWEEN 1 and 12
+
+$( '#amathyst', '#ruby', "#diamond", "quartz" ).click(function(event) {
+
+	 if (userClick == ruby) {
+         score++ +4
+        } else { (userClick == diamond)
+         score++ +6
+        } else { (userClick == amethyst)
+         score++ +2
+        } else { (userClick == quartz)
+         score++ +4
+      
+        }
+      });
+
+}
+    
+
+// <!-- compare the players score to the target score -->
+// while the players score is less than the target, they will keep playing. when the score is eaqual to the number, then it jumps out of the while loop
+
+
+compare the players score to the target score
+
+while (i < random) {
+  keep playing ?
+}
+
+
+
+
+// <!-- if then statement: -->
+
+if the random number equals the target score, you win
+if less than target score, click agin
+if over target score, you loose
+
+
+if (condition) {
+    block of code to be executed if the condition is true
+} else { 
+    block of code to be executed if the condition is false
+}
+
+
+// must enclose all code into a larger function so you can restart the game 
+
+}
+
+// now call the game so it starts over again
+game();
+
